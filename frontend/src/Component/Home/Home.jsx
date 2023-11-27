86d9cdca3aa5b25785eb7062078142d8cff69176
StanSNS <stanimirsergev159@gmail.com>
2023-11-28 00:57:29 +0200

    creating sorting and edit buttons on the frontend side.

--- frontend/src/Component/Home/Home.jsx
+++ frontend/src/Component/Home/Home.jsx
@@ -12,7 +12,13 @@ export default function Home() {
     const [users, setUsers] = useState([]); // State hook to manage the 'users' state variable and 'setUsers' function to update it.
     const [showModal, setShowModal] = useState(false); // State variable to control the visibility of a modal
     const [userToDelete, setUserToDelete] = useState(null); // State variable to store information about the user to be deleted
-
+    const [showSearchModal, setShowSearchModal] = useState(false); // State variable to control the visibility of the search modal
+    const [searchTerm, setSearchTerm] = useState(""); // New state for the search term
+    const [searchError, setSearchError] = useState(""); // New state for search error
+    const [showEditModal, setShowEditModal] = useState(false); // State variable to control the visibility of the edit user modal
+    const [editedUser, setEditedUser] = useState(null); // State variable to store information about the user being edited
+    const [newPhoneNumber, setNewPhoneNumber] = useState(""); // State variable to store the new phone number input in the edit user modal
+    const [phoneNumberError, setPhoneNumberError] = useState(""); // New state for phone number error
 
 
     // Function that handles the logout button click event.
@@ -73,12 +79,107 @@ export default function Home() {
             });
     };
 
+    // Handle the click event for default sorting
+    const handleDefaultSort = () => {
+        // Add logic for default sorting here
+        console.log("Default Sort Clicked");
+    };
+
+    // Handle the click event for sorting by last name and date of birth
+    const handleSortByLastName = () => {
+        console.log("Sort By Last Name and Date of Birth Clicked");
+    };
+
+    // Show the search modal when the "Search by Last Name" button is clicked
+    const handleSearchByLastName = () => {
+        setShowSearchModal(true);
+    };
+
+    // Close the search modal and reset the search term
+    const handleSearchModalClose = () => {
+        setSearchTerm("")
+        setShowSearchModal(false);
+    };
+
+    // Handle change in the search input field
+    const handleSearchInputChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    // Handle confirmation for search by last name
+    const handleSearchModalConfirm = () => {
+        if (searchTerm.trim() === "") {
+            setSearchError("Please enter a last name!");
+        } else {
+            setSearchError("");
+
+            console.log("Search by Last Name Confirmed:", searchTerm);
+            setShowSearchModal(false);
+        }
+    };
+
+    // Close the edit modal and reset related state variables
+    const closeEditModal = () => {
+        setShowEditModal(false);
+        setEditedUser(null);
+        setNewPhoneNumber("");
+        setPhoneNumberError("")
+    };
+
+    // Handle the click event for editing a user
+    const handleEditUser = (user) => {
+        setEditedUser(user);
+        setNewPhoneNumber(user.phoneNumber);
+        setShowEditModal(true);
+    };
+
+    // Confirm the edit of a user and close the edit modal
+    const confirmEditUser = () => {
+        if (newPhoneNumber.trim() === "") {
+            setPhoneNumberError("Please enter a phone number!");
+        } else {
+            setEditedUser((prevUser) => ({
+                ...prevUser,
+                phoneNumber: newPhoneNumber,
+            }));
+            closeEditModal();
+        }
+    };
+
+    // Handle the click event for getting a random user
+    const handleGetRandomUser = () => {
+        console.log("Get Random User Clicked");
+    };
 
     return (
         <>
             <div className="d-flex justify-content-between">
                 <div className="text-left font-weight-bolder ml-3 mt-1 ">
                     <h2>Hello, {loggedUserFirstName()}</h2>
+                </div>
+
+                <div className="mt-2">
+                    <button className="sortingButton"
+                            onClick={handleDefaultSort}>
+
+                        Default Sort
+                    </button>
+
+                    <button className="sortingButton ml-3 mr-3 "
+                            onClick={handleSortByLastName}>
+
+                        Sort By Last Name and Date of Birth
+                    </button>
+
+                    <button className="sortingButton"
+                            onClick={handleSearchByLastName}>
+                        Search by Last Name
+                    </button>
+
+                    <button className="sortingButton ml-3"
+                            onClick={handleGetRandomUser}>
+                        Random User
+                    </button>
                 </div>
 
                 <div className="text-right">
@@ -174,7 +275,14 @@ export default function Home() {
 
                                             <p className="reduceFontSize mb-2 ml-4"><span
                                                 className="orangeWordColor">private</span> String <span
-                                                className="pinkWordColor">phoneNumber</span>;</p>
+                                                className="pinkWordColor">phoneNumber</span>;
+
+                                                <button
+                                                    className="customEditButton ml-1 font-weight-bolder"
+                                                    onClick={() => handleEditUser(user)}>
+                                                    {'{EDIT}'}
+                                                </button>
+                                            </p>
 
                                             <p className="reduceFontSize mb-2 ml-4"><span
                                                 className="orangeWordColor">private</span> String <span
@@ -209,6 +317,7 @@ export default function Home() {
                     ))}
                 </div>
             </div>
+
             <Modal show={showModal} onHide={closeModal}>
                 <Modal.Header>
                     <Modal.Title>Confirmation</Modal.Title>
@@ -222,6 +331,60 @@ export default function Home() {
                     </Button>
                     <Button variant="danger" onClick={confirmDeleteUser}>
                         Delete
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+            <Modal show={showSearchModal} onHide={handleSearchModalClose}>
+                <Modal.Header>
+                    <Modal.Title>Search by Last Name</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <label htmlFor="lastNameInput">Enter Last Name:</label>
+                    <input
+                        type="text"
+                        id="lastNameInput"
+                        className="form-control"
+                        value={searchTerm}
+                        onChange={handleSearchInputChange}
+                    />
+                </Modal.Body>
+                {searchError && <p className="text-danger text-center font-weight-bolder">{searchError}</p>}
+
+                <Modal.Footer className="justify-content-center">
+
+                    <Button variant="secondary" onClick={handleSearchModalClose}>
+                        Cancel
+                    </Button>
+                    <Button variant="primary" onClick={handleSearchModalConfirm}>
+                        Confirm
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+            <Modal show={showEditModal} onHide={closeEditModal}>
+                <Modal.Header>
+                    <Modal.Title>Edit Phone Number</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <label htmlFor="phoneNumberInput">Enter New Phone Number:</label>
+                    <input
+                        type="text"
+                        id="phoneNumberInput"
+                        className="form-control"
+                        value={newPhoneNumber}
+                        onChange={(e) => setNewPhoneNumber(e.target.value)}
+                    />
+                </Modal.Body>
+                {phoneNumberError && (
+                    <p className="text-danger text-center font-weight-bolder">{phoneNumberError}</p>
+                )}
+                <Modal.Footer className="justify-content-center">
+                    <Button variant="secondary" onClick={closeEditModal}>
+                        Cancel
+                    </Button>
+                    <Button variant="primary" onClick={confirmEditUser}>
+                        Confirm
                     </Button>
                 </Modal.Footer>
             </Modal>
